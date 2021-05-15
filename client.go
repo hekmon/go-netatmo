fe7724823e1e4a6d5b0b34d28e9ca2fad01e280a
Hekmon <edouardhur@gmail.com>
2021-05-15 21:04:39 +0200

    add an helper to retreive the real auth url

--- client.go
+++ client.go
@@ -1,6 +1,12 @@
 package netatmo
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	cleanhttp "github.com/hashicorp/go-cleanhttp"
 	"golang.org/x/oauth2"
 )
 
@@ -16,28 +22,50 @@ type Client interface {
 	GetTokens() oauth2.Token
 }
 
-// OAuth2Config contains basic OAuth2 related information
+// OAuth2BaseConfig contains basic OAuth2 related information
 // in order to configure the full OAuth2 conf.
-type OAuth2Config struct {
+type OAuth2BaseConfig struct {
 	ClientID     string
 	ClientSecret string
 	Scopes       Scopes
-	RedirectURL  string // optional
+	RedirectURL  string // optional, if set it must match the one defined in the application profil on netatmo dev platform
 }
 
-// GetOfflineAuthURL returns a ready to use URL for oauth2 autorization against netatmo API
-func GetOfflineAuthURL(conf OAuth2Config, uniqID string) (url string) {
-	return GenerateOAuth2Config(conf).AuthCodeURL(uniqID, oauth2.AccessTypeOffline)
+func GetUserAuthorizationURL(ctx context.Context, conf OAuth2BaseConfig, uniqID string, customClient *http.Client) (userAuthURL *url.URL, err error) {
+	// Spawn an http client if necessary or clone the given one
+	if customClient == nil {
+		customClient = cleanhttp.DefaultClient()
+	} else {
+		clone := *customClient
+		customClient = &clone
+	}
+	// Deactivate redirect follow
+	customClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	// POST request expecting final URL to be on the 302 Location header response
+	generateURL := GenerateOAuth2Config(conf).AuthCodeURL(uniqID, oauth2.AccessTypeOffline)
+	resp, err := customClient.PostForm(generateURL, nil)
+	if err != nil {
+		err = fmt.Errorf("failed to recover the user auth URL using '%s' as base URL: %w", generateURL, err)
+		return
+	}
+	if resp.StatusCode != http.StatusFound {
+		err = fmt.Errorf("unexpected return code from '%s' was expecting %d: %s", generateURL, http.StatusFound, resp.Status)
+		return
+	}
+	return url.Parse(resp.Header.Get("Location"))
 }
 
 // GenerateOAuth2Config generates a prefilled OAuth2 conf for the Netatmo API
-func GenerateOAuth2Config(conf OAuth2Config) (oac *oauth2.Config) {
+func GenerateOAuth2Config(conf OAuth2BaseConfig) (oac *oauth2.Config) {
 	return &oauth2.Config{
 		ClientID:     conf.ClientID,
 		ClientSecret: conf.ClientSecret,
 		Endpoint: oauth2.Endpoint{
-			AuthURL:  NetatmoAPIAuthURL,
-			TokenURL: NetatmoAPITokenURL,
+			AuthURL:   NetatmoAPIAuthURL,
+			TokenURL:  NetatmoAPITokenURL,
+			AuthStyle: oauth2.AuthStyleInParams,
 		},
 		RedirectURL: conf.RedirectURL,
 		Scopes:      conf.Scopes.toStrSlice(),
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hekmon/go-netatmo
 
 go 1.16
 
-require golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
+require (
+	github.com/hashicorp/go-cleanhttp v0.5.2
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
+)

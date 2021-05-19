365d7ecf4f2cdfc58864383f528c677b77312c98
Hekmon <edouardhur@gmail.com>
2021-05-19 20:42:00 +0200

    adapt to new main pkg client

--- weather/client.go
+++ weather/client.go
@@ -2,12 +2,16 @@ package weather
 
 import "github.com/hekmon/go-netatmo"
 
+/*
+	https://dev.netatmo.com/apidocumentation/weather
+*/
 type WeatherClient struct {
-	client netatmo.Client
+	client netatmo.AuthenticatedClient
 }
 
-func New(client netatmo.Client) *WeatherClient {
+func New(client netatmo.AuthenticatedClient) *WeatherClient {
 	return &WeatherClient{
 		client: client,
 	}
 }
+
